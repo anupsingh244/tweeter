066a53c281f141c7925f987615031ed2fbed1bed
anupsingh244 <anup.singh@raweng.com>
2016-05-12 12:13:25 +0530

    savedtweet=localstorage

--- Tweeter/js/tweetservice.js
+++ Tweeter/js/tweetservice.js
@@ -11,39 +11,39 @@
     var deferred = $q.defer();
     var tweet=[];
     var vm=this;
- //   var savedtweet =$localStorage.message;
+    vm.savedtweet =$localStorage.message;
 
-    this.getfunc= function(){
+    vm.getfunc= function(){
      return $http.get('save.json')
      .then(function(response) { 
        tweet = response.data;
      //if there is no tweet then it will load some default tweets from JSON file 
      //and save to localstorage, otherwise load user tweets
-          if($localStorage.message=== undefined || $localStorage.message.length <= 0) 
+          if(vm.savedtweet=== undefined || vm.savedtweet.length <= 0) 
           {
-           $localStorage.message=tweet; 
-           return $localStorage.message;    
+           vm.savedtweet=tweet; 
+           return vm.savedtweet;    
          }
          else{
-      return $localStorage.message; //this will load tweets from localstorage
+      return vm.savedtweet; //this will load tweets from localstorage
       // this is to check git feature
     }
   });
 
    };
 
-   this.insert =function (text) {
+   vm.insert =function (text) {
     if(text !=''){
-      $localStorage.message.unshift({name:text});
-      deferred.resolve($localStorage.message);
+      vm.savedtweet.unshift({name:text});
+      deferred.resolve(vm.savedtweet);
       return deferred.promise;
     }
   };
 
-  this.delete= function (item){ 
-    var index=$localStorage.message.indexOf(item)
-    $localStorage.message.splice(index,1);    
-    deferred.resolve($localStorage.message);
+  vm.delete= function (item){ 
+    var index=vm.savedtweet.indexOf(item)
+    vm.savedtweet.splice(index,1);    
+    deferred.resolve(vm.savedtweet);
     return deferred.promise;
   };
 }
