b6d266431df9c8588aa62f1ce85e7b6ce237111c
anupsingh244 <anup.singh@raweng.com>
2016-05-12 17:36:40 +0530

    localstorage fix

--- Tweeter/js/tweetservice.js
+++ Tweeter/js/tweetservice.js
@@ -1,50 +1,54 @@
 (function() {
-    'use strict';
-    angular
-        .module('myApp')
-        .service('Service', Service);
+  'use strict';
+  angular
+  .module('myApp')
+  .service('Service', Service);
 
-    Service.$inject = ['$http', '$localStorage', '$q'];
-    //     /* @ngInject */
-    function Service($http, $localStorage, $q) {
+  Service.$inject = ['$http','$localStorage','$q'];
+   //     /* @ngInject */
+   function Service($http,$localStorage,$q) {
 
-        var deferred = $q.defer();
-        var tweet = [];
-        var vm = this;
-        vm.savedtweet = $localStorage.message;
+    var deferred = $q.defer();
+    var tweet=[];
+    var vm=this;
+  
+  // $localStorage.message=$localStorage.message;
 
-        vm.getfunc = function() {
-            return $http.get('save.json')
-                .then(function(response) {
-                    tweet = response.data;
-                    //if there is no tweet then it will load some default tweets from JSON file 
-                    //and save to localstorage, otherwise load user tweets
-                    if (vm.savedtweet.length <= 0) {
-                        vm.savedtweet = tweet;
-                        return vm.savedtweet;
-                    } else {
-                        return vm.savedtweet; //this will load tweets from localstorage
-                        // this is to check git feature
-                    }
-                });
-
-        };
-
-        vm.insert = function(text) {
-            if (text != '') {
-                vm.savedtweet.unshift({
-                    name: text
-                });
-                deferred.resolve(vm.savedtweet);
-                return deferred.promise;
-            }
-        };
-
-        vm.delete = function(item) {
-            var index = vm.savedtweet.indexOf(item)
-            vm.savedtweet.splice(index, 1);
-            deferred.resolve(vm.savedtweet);
-            return deferred.promise;
-        };
+    vm.getfunc= function(){
+     return $http.get('save.json')
+     .then(function(response) { 
+       tweet = response.data;
+     //if there is no tweet then it will load some default tweets from JSON file 
+     //and save to localstorage, otherwise load user tweets
+          if($localStorage.message=== undefined || $localStorage.message.length <= 0) 
+          {
+           $localStorage.message=tweet; 
+           return $localStorage.message;    
+         }
+         else{
+            console.log($localStorage.message);
+      return $localStorage.message; //this will load tweets from localstorage
+      // this is to check git feature
     }
+  });
+
+   };
+
+   vm.insert =function (text) {
+    if(text !=''){
+      $localStorage.message.unshift({name:text});
+      deferred.resolve($localStorage.message);
+      console.log($localStorage.message);
+      return deferred.promise;
+    }
+  };
+
+  vm.delete= function (item){ 
+    var index=$localStorage.message.indexOf(item)
+    $localStorage.message.splice(index,1);    
+    deferred.resolve($localStorage.message);
+    return deferred.promise;
+  };
+}
 })();
+
