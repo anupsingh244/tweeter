304c7eae2864426e19c779a49af62b637a80fb2a
anupsingh244 <anup.singh@raweng.com>
2016-05-12 15:01:29 +0530

    indentation fixing

--- Tweeter/js/controller.js
+++ Tweeter/js/controller.js
@@ -1,38 +1,36 @@
   (function() {
-    'use strict';
-    angular
-    .module("myApp")
-    .controller('FrmController',FrmController);
-    FrmController.$inject = ['Service'];
-    /* @ngInject */
-    function FrmController(Service){  
+      'use strict';
+      angular
+          .module("myApp")
+          .controller('FrmController', FrmController);
+      FrmController.$inject = ['Service'];
+      /* @ngInject */
+      function FrmController(Service) {
 
 
-  var vm=this;
-  vm.tweet=[];
+          var vm = this;
+          vm.tweet = [];
 
-  Service.getfunc().then(function(resp) {
-    vm.tweet = resp;
-  },
-  function(resp) {
-    console.log('tweets retrieval failed.')
-  });
+          Service.getfunc().then(function(resp) {
+                  vm.tweet = resp;
+              },
+              function(resp) {
+                  console.log('tweets retrieval failed.')
+              });
 
 
-  vm.btn_add= function(){
-    console.log(vm.txttweet);
-    Service.insert(vm.txttweet).then(function(data){
-      console.log(data,"controller");
-      vm.tweet = data;
-    });
-    vm.txttweet='';
+          vm.btn_add = function() {
+              Service.insert(vm.txttweet).then(function(data) {
+                  vm.tweet = data;
+              });
+              vm.txttweet = '';
+          };
 
-  };
+          vm.remove = function(item) {
+              Service.delete(item).then(function(data) {
+                  vm.tweet = data;
+              });
+          }
 
-  vm.remove= function(item){
-    Service.delete(item).then(function(data){
-      vm.tweet = data;
-    });
-  }
-    
-    }})();
+      }
+  })();
--- Tweeter/js/tweetservice.js
+++ Tweeter/js/tweetservice.js
@@ -1,51 +1,50 @@
 (function() {
-  'use strict';
-  angular
-  .module('myApp')
-  .service('Service', Service);
+    'use strict';
+    angular
+        .module('myApp')
+        .service('Service', Service);
 
-  Service.$inject = ['$http','$localStorage','$q'];
-   //     /* @ngInject */
-   function Service($http,$localStorage,$q) {
+    Service.$inject = ['$http', '$localStorage', '$q'];
+    //     /* @ngInject */
+    function Service($http, $localStorage, $q) {
 
-    var deferred = $q.defer();
-    var tweet=[];
-    var vm=this;
-    vm.savedtweet =$localStorage.message;
+        var deferred = $q.defer();
+        var tweet = [];
+        var vm = this;
+        vm.savedtweet = $localStorage.message;
 
-    vm.getfunc= function(){
-     return $http.get('save.json')
-     .then(function(response) { 
-       tweet = response.data;
-     //if there is no tweet then it will load some default tweets from JSON file 
-     //and save to localstorage, otherwise load user tweets
-          if(vm.savedtweet=== undefined || vm.savedtweet.length <= 0) 
-          {
-           vm.savedtweet=tweet; 
-           return vm.savedtweet;    
-         }
-         else{
-      return vm.savedtweet; //this will load tweets from localstorage
-      // this is to check git feature
+        vm.getfunc = function() {
+            return $http.get('save.json')
+                .then(function(response) {
+                    tweet = response.data;
+                    //if there is no tweet then it will load some default tweets from JSON file 
+                    //and save to localstorage, otherwise load user tweets
+                    if (vm.savedtweet === undefined || vm.savedtweet.length <= 0) {
+                        vm.savedtweet = tweet;
+                        return vm.savedtweet;
+                    } else {
+                        return vm.savedtweet; //this will load tweets from localstorage
+                        // this is to check git feature
+                    }
+                });
+
+        };
+
+        vm.insert = function(text) {
+            if (text != '') {
+                vm.savedtweet.unshift({
+                    name: text
+                });
+                deferred.resolve(vm.savedtweet);
+                return deferred.promise;
+            }
+        };
+
+        vm.delete = function(item) {
+            var index = vm.savedtweet.indexOf(item)
+            vm.savedtweet.splice(index, 1);
+            deferred.resolve(vm.savedtweet);
+            return deferred.promise;
+        };
     }
-  });
-
-   };
-
-   vm.insert =function (text) {
-    if(text !=''){
-      vm.savedtweet.unshift({name:text});
-      deferred.resolve(vm.savedtweet);
-      return deferred.promise;
-    }
-  };
-
-  vm.delete= function (item){ 
-    var index=vm.savedtweet.indexOf(item)
-    vm.savedtweet.splice(index,1);    
-    deferred.resolve(vm.savedtweet);
-    return deferred.promise;
-  };
-}
 })();
-
